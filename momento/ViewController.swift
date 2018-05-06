2dadd94e3c9a5a9f318eb87b29def2a94bad6c2c
Ali Noorani <arn_748@yahoo.co.in>
2018-05-05 21:22:44 -0700

    Initial Commit

---
+++ momento/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  momento
+//
+//  Created by Ali Noorani on 2018-05-05.
+//  Copyright © 2018 Ali Noorani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
